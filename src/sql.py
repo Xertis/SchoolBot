25c1497905439ccac34d5b00745e1d243d37f477
Xertis <118364459+Xertis@users.noreply.github.com>
2024-10-29 14:34:57 +0300

    Update sql.py

--- src/sql.py
+++ src/sql.py
@@ -29,21 +29,3 @@ class AsyncSQL:
         await self.connect()
         async with self.connection.execute(query, params or []) as cursor:
             return await cursor.fetchone()
-
-
-"""
-async def main():
-    db = AsyncSQL('database.db')
-    
-    await db.connect()  # Открываем соединение
-    await db.execute('INSERT INTO users (name, class, access_level, tg_id) VALUES (?, ?, ?, ?)', ('Камоза Оксана Евгеньевна', 43, 1, 1435393428))
-    
-    users = await db.fetchall('SELECT * FROM users')
-    print(users)
-    
-    await db.close()  # Закрываем соединение
-
-# Для запуска асинхронной функции
-import asyncio
-asyncio.run(main())
-"""
